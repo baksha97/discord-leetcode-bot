40d4712d007ed3f1b69d507bd25b6ac5a76d15cf
Alfayed Baksh <baksha97@gmail.com>
2023-01-27 12:39:06 -0500

    Develop (#1)
    * refactor
    
    * adjust query, logger
    
    * Update Dockerfile
    
    * Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,8 @@ WORKDIR /home/gradle/src
 RUN gradle build --no-daemon
 
 FROM openjdk:11
-
-EXPOSE 8080
-
 RUN mkdir /app
-
+EXPOSE 8080
 COPY --from=build /home/gradle/src/build/libs/*.jar /app/application.jar
 
 ENTRYPOINT ["java", "-jar","/app/application.jar"]
--- src/main/kotlin/main/leetcode/LeetcodeService.kt
+++ src/main/kotlin/main/leetcode/LeetcodeService.kt
@@ -1,104 +1,40 @@
 package main.leetcode
 
-
 import dev.kord.common.entity.Snowflake
-import io.github.crackthecodeabhi.kreds.connection.Endpoint
-import io.github.crackthecodeabhi.kreds.connection.newClient
-import io.ktor.client.HttpClient
-import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.logging.*
-import io.ktor.client.request.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.Json
-import main.Environment
 import me.jakejmattson.discordkt.annotations.Service
 
 
-interface LeetcodeRegistry {
-    suspend fun register(id: String, username: String)
-    suspend fun contains(id: String): Boolean
-    suspend fun get(id: String): String?
-    suspend fun remove(id: String)
-
-    suspend fun allUsers(): List<Pair<String, String>>
-
-    suspend fun <T> get(id: T) = get("$id")
-    suspend fun get(id: Snowflake) = get("${id.value}")
-}
-
 @Service
-class RedisLeetcodeRegistry : LeetcodeRegistry {
-    private val redis = newClient(Endpoint.from(Environment.redisUri))
+class LeetcodeService(
+    private val storage: LeetcodePersistence,
+    private val api: LeetcodeRemoteDatasource
+) {
 
-    override suspend fun register(id: String, username: String): Unit =
-        redis.use { client ->
-            client.set(id, username)
-        }
-
-
-    override suspend fun contains(id: String): Boolean =
-        redis.use { it.get(id) != null }
-
-    override suspend fun get(id: String): String? =
-        redis.use { it.get(id) }
-
-    override suspend fun remove(id: String): Unit =
-        redis.use { it.expire(id, 0u) }
-
-    override suspend fun allUsers(): List<Pair<String, String>> =
-        redis.use { client ->
-            client
-                .keys("*")
-                .map {
-                    Pair(it, client.get(it)!!)
-                }
-        }
-}
-
-
-@Service
-class LeetcodeService(private val registry: LeetcodeRegistry) {
-
-    private val client = HttpClient(CIO) {
-        install(Logging) {
-            logger = object: Logger {
-                override fun log(message: String) {
-                    println(message)
-                }
+    suspend fun getStatistics(username: String): Result<LeetcodeUser> =
+        api.getStatistics(username)
+            .onFailure {
+                println(it)
             }
-            level = LogLevel.HEADERS
-        }
-        install(ContentNegotiation) {
-            json(
-                Json {
-                    prettyPrint = true
-                    isLenient = true
-                }
-            )
-        }
-    }
 
-    suspend fun allStatistics(): List<Pair<String, LeetcodeStatistic>> =
-        registry.allUsers().map {
-            Pair(it.first, getStatistics(it.second))
-        }
-
-    suspend fun getStatistics(username: String): LeetcodeStatistic =
-        client.get {
-            url {
-                protocol = URLProtocol.HTTPS
-                host = "leetcode-stats-api.herokuapp.com"
-                appendPathSegments(username)
+    suspend fun allStatistics(): List<Pair<String, LeetcodeUser>> =
+        storage
+            .allUsers()
+            .toSet()
+            .map {
+                println(it.second)
+                val stats =
+                    api
+                    .getStatistics(it.second)
+                    .getOrNull()
+                        ?: return@map null
+                return@map Pair(it.first, stats)
             }
-        }.body()
+            .filterNotNull()
 
 
-    suspend fun register(id: String, username: String) = registry.register(id, username)
-    suspend fun get(id: String): String? = registry.get(id)
-    suspend fun remove(id: String) = registry.remove(id)
+    suspend fun register(id: String, username: String) = storage.register(id, username)
+    suspend fun get(id: String): String? = storage.get(id)
+    suspend fun remove(id: String) = storage.remove(id)
 
     suspend fun <T> get(id: T) = get("$id")
     suspend fun get(id: Snowflake) = get("${id.value}")
