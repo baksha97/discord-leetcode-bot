a88520925d69686173f869aa00d93e39d552688b
Alfayed <baksha97@gmail.com>
2023-01-26 19:51:39 -0500

    first commit

---
+++ src/main/kotlin/main/Commands.kt
@@ -0,0 +1,95 @@
+package main
+
+import dev.kord.common.Color
+import main.leetcode.LeetcodeService
+import main.leetcode.LeetcodeStatistic
+import me.jakejmattson.discordkt.arguments.AnyArg
+import me.jakejmattson.discordkt.commands.CommandSet
+import me.jakejmattson.discordkt.commands.commands
+import me.jakejmattson.discordkt.dsl.SlashResponder
+
+fun buildLeetcodeCommands(
+    service: LeetcodeService
+): CommandSet = commands("Leetcode") {
+
+    slash("Register", "Register your Leetcode") {
+        execute(AnyArg("username")) {
+            val id = "${author.id.value}"
+            service.register(id = id, args.first.trim())
+            respond("Successfully registered to your discord account.")
+        }
+    }
+
+    slash("Stats", "Get statistics for your leetcode account") {
+        execute {
+            runCatching {
+                val username = service.get(author.id)!!
+                val stats = service.getStatistics(username)
+                sendStatistic(username, stats)
+            }.onFailure {
+                when(it) {
+                    is NullPointerException ->
+                        respond("Register your leetcode account using `/register` to proceed.")
+                    else ->
+                        respond("Something went wrong: $it")
+                }
+            }
+        }
+    }
+
+    slash("Leaderboard", "Rankings based on Leetcode Rank") {
+        execute {
+            runCatching {
+                val usernamesAndStatistics = service.allStatistics()
+                sendLeaderboard(usernamesAndStatistics)
+            }.onFailure {
+                respond("Something went wrong: $it")
+            }
+        }
+    }
+}
+
+suspend fun SlashResponder.sendStatistic(username: String, statistic: LeetcodeStatistic) = respondPublic {
+    title = "$username statistics"
+    color = Color(0x00bfff)
+    url = "https://leetcode.com/$username/"
+
+    field("🎖 Ranking", false) {
+        "${statistic.ranking}"
+    }
+
+    field("Easy Solved", false) {
+        "\uD83D\uDFE2 ${statistic.easySolved}"
+    }
+    field("Medium Solved", false) {
+        "\uD83D\uDFE1 ${statistic.mediumSolved}"
+    }
+    field("Hard Solved", false) {
+        "\uD83D\uDD34 ${statistic.hardSolved}"
+    }
+
+    field("Total Solved", false) {
+        "\uD83D\uDFF0 ${statistic.totalSolved}"
+    }
+
+    field("Contribution Points", false) {
+        "\uD83E\uDDE9 ${statistic.contributionPoints}"
+    }
+
+    field("Acceptance Rate", false) {
+        "${statistic.acceptanceRate}%"
+    }
+}
+
+suspend fun SlashResponder.sendLeaderboard(data: List<Pair<String, LeetcodeStatistic>>) = respondPublic {
+    title = "Leaderboard"
+    color = Color(0x00bfff)
+
+    val leaderboard = data.sortedBy { pair -> pair.second.ranking }
+
+    leaderboard.forEachIndexed { index, (username, statistic) ->
+        field("{$index+1})", false) {
+            "$username | ${statistic.ranking}"
+        }
+    }
+}
---
+++ src/main/kotlin/main/Main.kt
@@ -0,0 +1,35 @@
+package main
+
+import dev.kord.common.annotation.KordPreview
+import io.github.cdimascio.dotenv.dotenv
+import io.github.crackthecodeabhi.kreds.connection.Endpoint
+import io.github.crackthecodeabhi.kreds.connection.newClient
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
+import main.leetcode.LeetcodeService
+import me.jakejmattson.discordkt.dsl.bot
+import me.jakejmattson.discordkt.locale.Language
+
+object Environment {
+    private val dotenv = dotenv()
+
+    val discordToken: String get() = dotenv.get("DISCORD_BOT_TOKEN")
+    val redisUri: String get() = dotenv.get("REDIS")
+}
+
+@OptIn(KordPreview::class)
+suspend fun main(args: Array<String>) {
+    bot(Environment.discordToken) {
+        onStart {
+            val guilds = kord.guilds.toList()
+            println("Guilds: ${guilds.joinToString { it.name }}")
+        }
+
+        localeOf(Language.EN) {
+            helpName = "Help"
+            helpCategory = "Utility"
+            commandRecommendation = "Recommendation: {0}"
+        }
+    }
+}
+
